399b10a244daee166b53b9bab84b4580b87d232b
Stalin Amagua <samagua@finanzas.gob.ec>
2026-05-12 11:50:42 -0500

    6-componentes-para-el-menu-lateral stalin

--- src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideMenuOptionsComponent } from '../../components/side-menu-options/side-menu-options.component';
+import { SideMenuHeaderComponent } from '../../components/side-menu-header/side-menu-header.component';
 
 @Component({
   selector: 'app-dashboard-page',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SideMenuHeaderComponent, SideMenuOptionsComponent],
   templateUrl: './dashboard-page.component.html'
 })
 export default class DashboardPageComponent {}
