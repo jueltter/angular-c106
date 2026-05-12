b7b06ed9539bbd717448a594aade8d37dccf42f2
Stalin Amagua <samagua@finanzas.gob.ec>
2026-05-12 11:59:36 -0500

    6-componentes-para-el-menu-lateral completed

--- src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SideMenuOptionsComponent } from '../../components/side-menu-options/side-menu-options.component';
-import { SideMenuHeaderComponent } from '../../components/side-menu-header/side-menu-header.component';
+import { SideMenuComponent } from '../../components/side-menu/side-menu.component';
 
 @Component({
   selector: 'app-dashboard-page',
-  imports: [RouterOutlet, SideMenuHeaderComponent, SideMenuOptionsComponent],
+  imports: [RouterOutlet, SideMenuComponent],
   templateUrl: './dashboard-page.component.html'
 })
 export default class DashboardPageComponent {}
