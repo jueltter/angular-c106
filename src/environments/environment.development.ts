5e9251c9f870ab7ea43ea32c9d2747b6e2950991
Stalin Amagua <samagua@finanzas.gob.ec>
2026-05-13 12:05:19 -0500

    6-angular-environments-y-path-alias completed

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  companyName: 'Your Company Name',
+  companyName2: 'Your Company Name 2',
+  companySlogan: 'Your Company Slogan',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  companyName: 'Your Company Name',
+  companyName2: 'Your Company Name 2',
+  companySlogan: 'Your Company Slogan',
+
+};
